87e25bdfb79475828abb26764fc8117d203fa7e5
Guilherme <bastosguilherme12@gmail.com>
2022-08-24 08:36:39 -0300

    Old Version

---
+++ Look4MeAPI/Controllers/SobrenomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Look4MeAPI.Controllers
+{
+    public class SobrenomeController : ApiController
+    {
+        // GET: api/Sobrenome
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Sobrenome/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Sobrenome
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Sobrenome/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Sobrenome/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Look4MeAPI/Models/Sobrenome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Look4MeAPI.Models
+{
+    public class Sobrenome
+    {
+
+        public int idsobrenome { get; set; }
+        public string nome { get; set; }
+        public string pais1 { get; set; }
+        public string pais2 { get; set; }
+        public string pais3 { get; set; }
+        public string origem { get; set; }
+        public string significado { get; set; }
+
+    }
+}
