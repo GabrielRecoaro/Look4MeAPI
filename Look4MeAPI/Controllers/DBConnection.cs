cfe375b84e40315a951133c374bcc6a6fd4530e7
Guilherme <bastosguilherme12@gmail.com>
2022-08-25 12:14:15 -0300

    Connection DB

---
+++ Look4MeAPI/Controllers/DBConnection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Look4MeAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Look4MeAPI.Controllers
+{
+    public class DBConnection
+    {
+    
+        MySqlConnection conn2;
+
+        static string host = "localhost";
+
+        static string database = "dbLook4Me";
+
+        static string userDB = "root";
+
+        static string password = "12345678";
+
+        public static string strProvider = "server=" + host +
+                                            ";Database=" + database +
+                                            ";User ID=" + userDB +
+                                            ";Password=" + password;
+
+        public static Boolean novo = false;
+        public String sql;
+
+        public DBConnection()
+        {
+
+            conn2 = new MySqlConnection(strProvider);
+
+            conn2.Open();
+
+        }
+        public List<Sobrenome> BuscaTodos()
+        {
+
+            MySqlDataReader reader;
+
+            sql = "SELECT * FROM tbSobrenome;";
+            MySqlCommand cmd = new MySqlCommand(sql, conn2);
+
+            reader = cmd.ExecuteReader();
+            List<Sobrenome> l = new List<Sobrenome>();
+
+            if (reader.HasRows)
+            {
+  
+                while (reader.Read())
+                {
+                    l.Add(new Sobrenome(int.Parse(reader["IDSobrenome"].ToString()), reader["Nome"].ToString(), reader["Pais1"].ToString(), reader["Pais2"].ToString(), reader["Pais3"].ToString(), reader["Arvore"].ToString()));
+
+                }
+            }
+            return l;
+
+        }
+
+        public void Fechar()
+        {
+            conn2.Close();
+        }
+    }
+}
--- Look4MeAPI/Models/Sobrenome.cs
+++ Look4MeAPI/Models/Sobrenome.cs
@@ -7,14 +7,27 @@ namespace Look4MeAPI.Models
 {
     public class Sobrenome
     {
+        public Sobrenome()
+        {
 
+        }
+
+        public Sobrenome(int idsobrenome, string nome, string pais1, string pais2, string pais3, string arvore)
+        {
+            IDSobrenome = idsobrenome;
+            Nome = nome;
+            Pais1 = pais1;
+            Pais2 = pais2;
+            Pais3 = pais3;
+            Arvore = arvore;
+        }
         public int idsobrenome { get; set; }
         public string nome { get; set; }
         public string pais1 { get; set; }
         public string pais2 { get; set; }
         public string pais3 { get; set; }
-        public string origem { get; set; }
-        public string significado { get; set; }
+        public string arvore { get; set; }
+
 
     }
 }
